bba01e9746cc13a4eae18f890c1162ba05b7218f
Martin Radnev <127389329+MartinR260@users.noreply.github.com>
2025-06-23 19:58:34 +0300

    Added Games

---
+++ Shooters/shooter/game.js
@@ -0,0 +1,110 @@
+﻿// Creating variables
+var myX = 0, myY = 0, bulletX=0, bulletY=0, AlienX=[0,1,2],AlienY=[0,1,2],skorost=2,jivoti=27,tochki=0, winpointsLeft=19,vragjivot=3;
+
+function update() {
+     for(i=0;i<3;i++){
+     AlienX[i]=AlienX[i]-skorost;
+     if(AlienY[i]>500){
+     AlienY[i]=450;
+     }
+     if(AlienY[i]<0){
+     AlienY[i]=50;
+     }
+     }
+    myX = myX+(mouseX-myX)/10;
+    myY = myY+(mouseY-myY)/10;
+    if(myX>1){
+    myX=1;
+    }
+    if(myY>570){
+    myY=570;
+    }
+    bulletX=bulletX+12;
+    if(bulletX>800){
+    bulletX=0;
+    bulletY=myY;
+    }
+for(iV=0;iV<3;iV++){
+if(AlienX[iV]<0){
+AlienY[iV]=Math.random()*600;
+AlienX[iV]=1000;
+skorost=skorost+1;
+jivoti=jivoti-1;
+}
+if(areColliding(bulletX,bulletY,18,13,AlienX[iV],AlienY[iV],30,30)){
+vragjivot=vragjivot-1;
+bulletX=0;
+bulletY=myY;
+}
+if(vragjivot<=0){
+AlienY[iV]=Math.random()*600;
+AlienX[iV]=1000;
+skorost=skorost-1;
+tochki=tochki+1;
+winpointsLeft=winpointsLeft-1;
+vragjivot=3;
+}
+}
+
+           
+if(skorost>6){
+skorost=6;
+}
+if(skorost<4){
+skorost=4;
+}
+}
+
+
+function draw() {
+    // This is how you draw a rectangle
+     context.fillStyle="#266268";
+    context.fillRect(0,0,800,600);
+    context.fillStyle="yellow";
+    context.fillRect(myX,myY,30,30);
+    context.fillStyle="black";
+    context.fillRect(bulletX,bulletY,18,13);
+    context.fillStyle="orange";
+    context.fillRect(AlienX[0],AlienY[0],30,30);
+    context.fillRect(AlienX[1],AlienY[1],30,30);
+    context.fillRect(AlienX[2],AlienY[2],30,30);
+    context.fillStyle="brown";
+    context.font='20px Title';
+    context.fillText("Lives:",86,540);
+    context.fillText(jivoti,136,540);
+    context.fillText("Points:",626,540);
+    context.fillText(tochki,686,540);
+context.fillStyle="yellow";
+context.font='40px Title';
+context.fillText("points to win:",100,200);
+context.fillText(winpointsLeft,320,200);
+    if(jivoti<1){
+context.fillStyle="black";
+context.fillRect(0,0,800,600);
+context.fillStyle="red";
+context.font='100px Snap ITC';
+context.fillText("YOU DIED!",130,300);
+context.font='30px Snap ITC';
+context.fillText("Try again(by clicking refresh)!",130,400);
+bulletY=1000;
+}
+if(tochki>=19){
+context.fillStyle="#266268";
+context.fillRect(0,0,800,600);
+context.fillStyle="yellow";
+context.font='100px Ravie';
+context.fillText("YOU WON!",130,300);
+AlienX[0]=AlienX[0]+200;
+AlienX[1]=AlienX[1]+200;
+AlienX[2]=AlienX[2]+200;
+}
+};
+function keyup(key) {
+    // Show the pressed keycode in the console
+    console.log("Pressed", key);
+};
+
+function mouseup() {
+    // Show coordinates of mouse on click
+    console.log("Mouse clicked at", mouseX, mouseY);
+};
---
+++ Shooters/shooter_hardcore/game.js
@@ -0,0 +1,134 @@
+﻿// Creating variables
+var myX = 0, myY = 0, bulletX=0, bulletY=0, AlienX=1000,AlienY=Math.random()*600,Alien2X=1200,Alien3X=1300,Alien3Y=Math.random()*300,Alien4X=1400,Alien4Y=Math.random()*200;
+var Alien2Y=Math.random()*400,skorost=4,jivoti=8,tochki=0, winpointsLeft=30;
+
+function update() {
+     Alien4X=Alien4X-skorost;
+     Alien3X=Alien3X-skorost;
+     Alien2X=Alien2X-skorost;
+     AlienX=AlienX-skorost;
+    myX = myX+(mouseX-myX)/10;
+    myY = myY+(mouseY-myY)/10;
+    if(myX>1){
+    myX=1;
+    }
+    if(myY>570){
+    myY=570;
+    }
+    bulletX=bulletX+14;
+    if(bulletX>800){
+    bulletX=0;
+    bulletY=myY;
+    }
+if(AlienX<0){
+AlienY=Math.random()*600;
+AlienX=1000;
+skorost=skorost+1;
+jivoti=jivoti-1;
+}
+if(areColliding(bulletX,bulletY,18,13,AlienX,AlienY,30,30)){
+AlienY=Math.random()*600;
+AlienX=1000;
+skorost=skorost-1;
+tochki=tochki+1;
+winpointsLeft=winpointsLeft-1;
+}
+           if(Alien2X<0){
+           Alien2Y=Math.random()*400;
+           Alien2X=1200;
+           skorost=skorost+1;
+           jivoti=jivoti-1;
+           }
+           if(areColliding(bulletX,bulletY,18,13,Alien2X,Alien2Y,30,30)){
+           Alien2Y=Math.random()*400;
+           Alien2X=1200;
+           skorost=skorost-1; 
+           tochki=tochki+1;
+           winpointsLeft=winpointsLeft-1; 
+           }
+if(Alien3X<0){
+Alien3Y=Math.random()*300;
+Alien3X=1200;
+skorost=skorost+1;
+jivoti=jivoti-1;
+}
+if(areColliding(bulletX,bulletY,18,13,Alien3X,Alien3Y,30,30)){
+Alien3Y=Math.random()*300;
+Alien3X=1200;
+skorost=skorost-1; 
+tochki=tochki+1;
+winpointsLeft=winpointsLeft-1;
+}
+     if(Alien4X<0){
+    Alien4Y=Math.random()*200;
+   Alien4X=1200;
+  skorost=skorost+1;
+ jivoti=jivoti-1;
+}
+if(areColliding(bulletX,bulletY,18,13,Alien4X,Alien4Y,30,30)){
+Alien4Y=Math.random()*200;
+Alien4X=1200;
+skorost=skorost-1; 
+tochki=tochki+1;
+winpointsLeft=winpointsLeft-1;
+}
+if(skorost>5){
+skorost=5;
+}
+if(skorost<3){
+skorost=3;
+}
+}
+
+
+function draw() {
+    // This is how you draw a rectangle
+     context.fillStyle="black";
+    context.fillRect(0,0,800,600);
+    context.fillStyle="yellow";
+    context.fillRect(myX,myY,30,30);
+    context.fillStyle="grey";
+    context.fillRect(bulletX,bulletY,18,13);
+    context.fillStyle="red";
+    context.fillRect(AlienX,AlienY,30,30);
+    context.fillRect(Alien2X,Alien2Y,30,30);
+    context.fillRect(Alien3X,Alien3Y,30,30);
+    context.fillRect(Alien4X,Alien4Y,30,30);
+    context.fillStyle="brown";
+    context.font='20px Title';
+    context.fillText("Lives:",86,540);
+    context.fillText(jivoti,136,540);
+    context.fillText("Points:",626,540);
+    context.fillText(tochki,686,540);
+context.fillStyle="yellow";
+context.font='40px Title';
+context.fillText("points to win:",100,200);
+context.fillText(winpointsLeft,320,200);
+    if(jivoti<1){
+context.fillStyle="black";
+context.fillRect(0,0,800,600);
+context.fillStyle="red";
+context.font='100px Title';
+context.fillText("YOU DIED!",130,300);
+context.font='30px Title';
+context.fillText("Try again(by clicking refresh)!",130,400);
+bulletY=1000;
+}
+if(tochki==30){
+context.fillStyle="#266268";
+context.fillRect(0,0,800,600);
+context.fillStyle="yellow";
+context.font='100px Title';
+context.fillText("YOU WON!",130,300);
+bulletY=1000;
+}
+};
+function keyup(key) {
+    // Show the pressed keycode in the console
+    console.log("Pressed", key);
+};
+
+function mouseup() {
+    // Show coordinates of mouse on click
+    console.log("Mouse clicked at", mouseX, mouseY);
+};
